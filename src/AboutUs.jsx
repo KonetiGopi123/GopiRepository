c867f5ebca096a9fd376d1b07ffaab553871b76f
gopi koneti <gopikoneti12345@gmail.com>
2024-11-02 12:34:04 +0530

    modification

--- src/AboutUs.jsx
+++ src/AboutUs.jsx
@@ -2,7 +2,7 @@ function AboutUs()
 {
     return(
         <>
-            <h1>This page belongs to aboutus</h1>
+        <h1>about us page</h1>
         <h1>This is AboutUs page</h1>
         
         
